d2afd59131e0ca82193b2f447e98fd0d6a67326b
fernandogelin <fernandogelin@gmail.com>
2018-10-04 10:04:05 -0400

    adds mkdocs and mkdocs material versions;

--- docs/make.jl
+++ docs/make.jl
@@ -3,7 +3,7 @@ using Documenter
 makedocs()
 
 deploydocs(
-    deps   = Deps.pip("mkdocs", "python-markdown-math", "mkdocs-material"),
+    deps   = Deps.pip("mkdocs==0.17.5", "mkdocs-material==2.9.4"),
     repo = "github.com/compbiocore/ViVa.jl.git",
     julia  = "0.6",
     osname = "linux"
