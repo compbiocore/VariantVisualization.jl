ee191844e4bbc953b70174bd8a47b7e807cc0059
fernandogelin <fernandogelin@gmail.com>
2019-03-01 17:28:48 -0500

    docs(docs):

--- docs/make.jl
+++ docs/make.jl
@@ -4,7 +4,7 @@ makedocs(sitename="docs")
 
 deploydocs(
     repo   = "github.com/compbiocore/VIVA.jl.git",
-    deps   = Deps.pip("mkdocs", "mkdocs-material", "pygments"),
+    deps   = Deps.pip("mkdocs==1.0.4", "mkdocs-material==4.0.2", "pygments"),
     make   = () -> run(`mkdocs build`),
     target = "site"
 )
