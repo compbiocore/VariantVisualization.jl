c6b76dbced6052f34081f531908c2c5fb0efc334
fernandogelin <fernandogelin@gmail.com>
2019-03-01 16:55:24 -0500

    docs(mkdocs):

--- docs/make.jl
+++ docs/make.jl
@@ -1,8 +1,10 @@
 using Documenter, VIVA
 
+makedocs(sitename="docs")
+
 deploydocs(
     repo   = "github.com/compbiocore/VIVA.jl.git",
-    deps   = Deps.pip("mkdocs", "pygments", "python-markdown-math"),
-    make   = () -> run(`mkdocs build`)
+    deps   = Deps.pip("mkdocs", "mkdocs-material", "pygments"),
+    make   = () -> run(`mkdocs build`),
     target = "site"
 )
