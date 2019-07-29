dcdaadc15625d3698e4438e91375c7f463e66e90
Gelin <fgelin@ad.brown.edu>
2019-07-29 16:48:04 -0400

    build script for windows

--- deps/build.jl
+++ deps/build.jl
@@ -1,18 +1,26 @@
 function get_executable_path(package::AbstractString, exec::AbstractString)
     base_path = Base.find_package(package)
-    exec_path = joinpath(split(base_path, "src/$package.jl")[1], exec)
+    sep = joinpath("src", "$package.jl")
+    exec_path = joinpath(split(base_path, sep)[1], exec)
 
     return exec_path
 end
 
 function symlink_user_bin(path_::AbstractString)
-    exec = split(path_, "/")[end]
     if Sys.iswindows()
-        symlink(path_, "C:\\ProgramData\\Bin\\$exec")
+        exec = split(path_, "\\")[end]
+        try
+          bin_path = split(path_, "\\$exec")[1]
+          @warn bin_path
+          run(`setx path "%path%;$bin_path"`)
+        catch
+          @warn bin_path
+        end
     else
+        exec = split(path_, "/")[end]
         symlink(path_, "/usr/local/bin/$exec")
+        @warn "Created symlink: /usr/local/bin/viva -> $path_"
     end
-    @info "Created symlink: /usr/local/bin/viva -> $path_"
 end
 
 path_ = get_executable_path("VariantVisualization", "viva")
